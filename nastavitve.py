b950e74dfa9038ef9148a7442bf3989dbf5afbb3
nejc <nejc.mobilc@gmail.com>
2019-05-01 12:25:06 +0200

    tretja različica, manjši popravki, dodan nastavek za argparse

--- nastavitve.py
+++ nastavitve.py
@@ -1,5 +1,65 @@
-#!/usr/bin/env python
+#! python3
 # -*- coding: 'UTF-8' -*-
+
+import configparser
+import os
+
 
 client_id = '82013fb3a531d5414f478747c1aca622'
 session_id = '5cc765368952d4.22094713.245769764'
+
+
+def ustvari_nastavitve(config, ime_datoteke='nastavitve.ini'):
+    lokacija = os.getcwd()
+    client_id = input('Vnesite identifikacijsko številko uporabnika.\n')
+    session_id = input('Vnesite identifikacijsko število seje.\n')
+    naslov = input('Vnesite lokacijo shrambe.\n')
+    predvajalnik = input('Vnestite ime predvajalnika (npr.: mpv).')
+    možnosti = input(
+        'Vnesite dodatne možnosti za predvajalnik (npr.: --force-window)')
+
+    config['PROGRAM'] = {'lokacija': lokacija}
+
+    config['DOSTOP'] = {'client_id': client_id,
+                        'session_id': session_id}
+
+    config['SHRAMBA'] = {'naslov': naslov}
+
+    # nastavek za predvajanje posnetka
+    # config['PREDVAJANJE'] = {'predvajalnik': predvajalnik,
+    #                          'možnosti': možnosti_predvajalnika}
+
+    with open(ime_datoteke, 'w') as f:
+        config.write(f)
+
+
+def naloži_nastavitve(ime_datoteke='nastavitve.ini'):
+    config = configparser.ConfigParser()
+    config.read(ime_datoteke)
+
+    nastavitve = {}
+
+    for i in config.sections():
+        nastavitve.update(dict(config[i]))
+
+    return nastavitve
+
+
+def main():
+    ime_datoteke = 'nastavitve.ini'
+    config = configparser.ConfigParser()
+
+    if not os.path.exists(ime_datoteke):
+        print('Nisem našel nastavitev. Morate jih ustvariti.')
+        ustvari_nastavitve(config)
+
+    else:
+        print(f'''Upoštevam obstoječe nastavitve v datoteki \
+{os.getcwd()}/{ime_datoteke}.\nČe želite ustvariti novo datoteko z \
+nastavitvami, najprej izbrišite obstoječo datoteko z ukazom: \n\trm \
+{os.getcwd()}/{ime_datoteke} \nNato ponovno zaženite program.
+            ''')
+
+
+if __name__ == '__main__':
+    main()
--- rtvslo.py
+++ rtvslo.py
@@ -7,44 +7,50 @@ import re
 import pyperclip
 import requests
 import sys
+# import argparse
+# import subprocess
+
+# za zaganjanje programa izven domače mape
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 
-def pridobi_api(povezava_do_html):
+def pridobi_api(povezava_do_html, n={}):
     '''
     vhod: povezava spletne strani, na kateri je posnetek 
     izhod: povezava do API, id posnetka [tuple]
     '''
     # 4d.rtvslo.si
-    štiride = re.compile(r'https://4d\.rtvslo\.si/arhiv/\w+/\d{9,}')
+    štiride = re.compile(r'https://4d\.rtvslo\.si/arhiv/\S+/\d{9,}')
     # radioprvi.rtvslo.si, val202.rtvslo.si, ars.rtvslo.si
     ostali = re.compile(r'^https://(ars|prvi|radioprvi|val202)\.rtvslo\.si/')
 
     if štiride.search(povezava_do_html):
         # zadnjih 9 števk je id, morda bo treba v prihodnosti to spremeniti
         številka = štiride.search(povezava_do_html).group()[-9:]
-        povezava_do_api = f'https://api.rtvslo.si/ava/getRecording/{številka}?client_id={nastavitve.client_id}'
+        povezava_do_api = f"https://api.rtvslo.si/ava/getRecording/{številka}?client_id={n['client_id']}"
         return (povezava_do_api, številka)
     elif ostali.search(povezava_do_html):
         r = requests.get(povezava_do_html)
         iskanje_povezave = re.compile(
-            r'https:\/\/4d\.rtvslo\.si\/arhiv\/\S+\d+')
+            r'https://4d\.rtvslo\.si/arhiv/\S+\d+')
         # zadnjih 9 števk je id, morda bo treba v prihodnosti to spremeniti
         številka = iskanje_povezave.search(r.text).group()[-9:]
-        povezava_do_api = f'https://api.rtvslo.si/ava/getRecording/{številka}?client_id={nastavitve.client_id}'
+        povezava_do_api = f"https://api.rtvslo.si/ava/getRecording/{številka}?client_id={n['client_id']}"
 
         return (povezava_do_api, številka)
 
 
-def pridobi_json(povezava_do_api):
+def pridobi_json(povezava_do_api, n={}):
     '''
     vhod: povezava do API
     izhod: JSON s podatki o posnetku 
-    funkcija pridobi informacije v obliki JSON o posnetku z api.rtvslo.si
+    ukaz pridobi informacije v obliki JSON o posnetku z api.rtvslo.si
     '''
     r = requests.get(povezava_do_api)
     # za posnetke, ki zahtevajo prijavo
-    if  'prijava' in json.loads(r.text)['response']['mediaFiles'][0]['filename']:
-        povezava_do_api += f'&session_id={nastavitve.session_id}'
+    if 'prijava' in json.loads(r.text)['response']['mediaFiles'][0]['filename']:
+        povezava_do_api += f"&session_id={n['session_id']}"
         r = requests.get(povezava_do_api)
     return json.loads(r.text)['response']
 
@@ -57,7 +63,7 @@ def info_json(džejsn):
     try:
         # 4d.rtvslo.si
         streamer = džejsn['mediaFiles'][1]['streamers']['http'].rstrip('/')
-        filename = džejsn['mediaFiles'][1]['filename']
+        filename = džejsn['mediaFiles'][1]['filename'].lstrip('/')
     except IndexError:
         # radioprvi.rtvslo.si, val202.rtvslo.si, ars.rtvslo.si
         streamer = džejsn['mediaFiles'][0]['streamers']['http'].rstrip('/')
@@ -66,32 +72,60 @@ def info_json(džejsn):
         povezava_do_posnetka = f'{streamer}/{filename}'
 
     ime_posnetka = džejsn['title'].replace(' ', '-').lower()
-    opis_posnetka = džejsn['showDescription']
+    try:
+        opis_posnetka = džejsn['showDescription']
+    except KeyError as e:
+        print('Ne najdem opisa posnetka.', e)
+        opis_posnetka = None
     return (povezava_do_posnetka, ime_posnetka, opis_posnetka)
 
 
-def prenesi_posnetek(informacije):
+def shrani_posnetek(informacije):
     '''
     vhod: informacije o posnetku: povezava do posnetka, ime, opis [tuple]
-    izhod: posnetek 
+    izhod: posnetek
     '''
     r = requests.get(informacije[0])
     with open(f'{informacije[1][:21]}.mp4', 'w+b') as f:
         f.write(r.content)
 
 
+def pridobi_posnetek(povezava_do_html, n):
+    '''
+    vhod: povezava spletne strani, na kateri je posnetek
+    izhod: informacije o posnetku: povezava do posnetka, ime, opis [tuple]
+    gre za metaukaz, ki veriži pridobi_api, pridobi_json in info_json
+    '''
+    povezava_do_api = pridobi_api(povezava_do_html, n)[0]
+    informacije = info_json(pridobi_json(povezava_do_api, n))
+    return informacije
+
+# TODO
+# def argumenti():
+#     p = argparse.ArgumentParser(
+#         description='Program, ki dostopa do posnetkov na portalu rtvslo.si\
+#                     Transmission.'
+#     )
+#     p.add_argument('-p', '--predvajaj', type=str,
+#                    help='predvajaj posnetek v predvajalniku')
+#     p.add_argument('-s', '--shrani', type=str,
+#                    help='predvajaj posnetek v predvajalniku')
+
+# nastavek za predvajanje posnetka
+# def predvajaj_posnetek(informacije, n):
+#     subprocess.call([n['predvajalnik'], informacije[0], n['možnosti']])
+
+
 def main():
+    # args = argumenti()
+    n = nastavitve.naloži_nastavitve()
     try:
         povezava_do_html = sys.argv[1].lower()
     except IndexError:
         povezava_do_html = pyperclip.paste().lower()
     finally:
-        try:
-            povezava_do_api = pridobi_api(povezava_do_html)[0]
-            informacije = info_json(pridobi_json(povezava_do_api))
-            prenesi_posnetek(informacije)
-        except (AttributeError, TypeError):
-            print('Neveljavna povezava.')
+        informacije = pridobi_posnetek(povezava_do_html, n)
+        shrani_posnetek(informacije)
 
 
 if __name__ == '__main__':
