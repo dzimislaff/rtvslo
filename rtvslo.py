6299f501f295e3daf5c0bd363349e9586746e3c3
nejc <nejc.mobilc@gmail.com>
2019-07-15 23:01:32 +0200

    dodano predvajanje programa v živo; odstranjene nedelujoče povezave

--- rtvslo.py
+++ rtvslo.py
@@ -21,25 +21,33 @@ def pridobi_api(povezava_do_html, n={}):
     vhod: povezava spletne strani, na kateri je posnetek 
     izhod: povezava do API, id posnetka [tuple]
     '''
-    # 4d.rtvslo.si
-    štiride = re.compile(r'https?://4d\.rtvslo\.si/arhiv/\S+/\d{9,}')
-    # radioprvi.rtvslo.si, val202.rtvslo.si, ars.rtvslo.si
-    ostali = re.compile(r'^https://(ars|prvi|radioprvi|val202)\.rtvslo\.si/')
+    # 4d.rtvslo.si/arhiv
+    štiride = re.compile(r'https?://4d\.rtvslo\.si/arhiv/\S+/\d{7,11}')
+    # 4d.rtvslo.si/zivo
+    v_živo = re.compile(r'https?://4d\.rtvslo\.si/zivo/\S+')
+    cifra = re.compile(r'\d{7,11}')
 
     if štiride.search(povezava_do_html):
         # zadnjih 9 števk je id, morda bo treba v prihodnosti to spremeniti
-        številka = štiride.search(povezava_do_html).group()[-9:]
+        številka = cifra.search(povezava_do_html).group()
         povezava_do_api = f"https://api.rtvslo.si/ava/getRecording/{številka}?client_id={n['client_id']}"
-        return (povezava_do_api, številka)
-    elif ostali.search(povezava_do_html):
-        r = requests.get(povezava_do_html)
-        iskanje_povezave = re.compile(
-            r'https://4d\.rtvslo\.si/arhiv/\S+\d+')
-        # zadnjih 9 števk je id, morda bo treba v prihodnosti to spremeniti
-        številka = iskanje_povezave.search(r.text).group()[-9:]
-        povezava_do_api = f"https://api.rtvslo.si/ava/getRecording/{številka}?client_id={n['client_id']}"
-
-        return (povezava_do_api, številka)
+    elif v_živo.search(povezava_do_html):
+        pari = {'tvs1': 'tv.slo1',
+                'tvs2': 'tv.slo2',
+                'tvs3': 'tv.slo3',
+                'tvkp': 'tv.kp1',
+                'tvmb': 'tv.mb1',
+                'tvmmc': 'tv.mmctv'}
+                # nedelujoče povezave
+                # 'ra1': 'ra.a1',
+                # 'val202': 'ra.val201',
+                # 'ars': 'ra.ars',
+                # 'rasi': 'ra.rsi'
+        for i in pari.keys():
+            if i in povezava_do_html:
+                povezava_do_api = f"https://api.rtvslo.si/ava/getLiveStream/{pari[i]}?client_id={n['client_id']}"
+                break
+    return povezava_do_api
 
 
 def pridobi_json(povezava_do_api, n):
@@ -49,10 +57,10 @@ def pridobi_json(povezava_do_api, n):
     ukaz pridobi informacije v obliki JSON o posnetku z api.rtvslo.si
     '''
     r = requests.get(povezava_do_api)
-    # za posnetke, ki zahtevajo prijavo
-    if 'prijava' in json.loads(r.text)['response']['mediaFiles'][0]['filename']:
-        povezava_do_api += f"&session_id={n['session_id']}"
-        r = requests.get(povezava_do_api)
+    # za posnetke, ki zahtevajo prijavo – začasno umaknjeno
+    # if 'prijava' in json.loads(r.text)['response']['mediaFiles'][0]['filename']:
+    #     povezava_do_api += f"&session_id={n['session_id']}"
+    #     r = requests.get(povezava_do_api)
     return json.loads(r.text)['response']
 
 
@@ -75,17 +83,15 @@ def info_json(džejsn, n):
     izhod: informacije o posnetku: povezava do posnetka, ime, opis [tuple]
     '''
     try:
-        # 4d.rtvslo.si
-        streamer = džejsn['mediaFiles'][1]['streamers']['http'].rstrip('/')
-        filename = džejsn['mediaFiles'][1]['filename'].lstrip('/')
-        mediatype = džejsn['mediaFiles'][0]['mediaType'].lower()
-    except IndexError:
-        # radioprvi.rtvslo.si, val202.rtvslo.si, ars.rtvslo.si
+        # arhiv
         streamer = džejsn['mediaFiles'][0]['streamers']['http'].rstrip('/')
         filename = džejsn['mediaFiles'][0]['filename'].lstrip('/')
-        mediatype = džejsn['mediaFiles'][0]['mediaType'].lower()
-    finally:
-        povezava_do_posnetka = f'{streamer}/{filename}'
+    except (KeyError, TypeError):
+        # živo
+        streamer = džejsn['mediaFiles'][0]['streamer'].rstrip('/')
+        filename = džejsn['mediaFiles'][0]['file'].lstrip('/').rstrip('?sub=0')
+    mediatype = džejsn['mediaFiles'][0]['mediaType'].lower()
+    povezava_do_posnetka = f'{streamer}/{filename}'
 
     ime = džejsn['title'].replace(' ', '-').lower()
     ime = odstrani_znake(ime, n['znaki'].split(','))
@@ -103,6 +109,8 @@ def shrani_posnetek(informacije, n):
     vhod: informacije o posnetku: povezava do posnetka, ime, opis [tuple]
     izhod: posnetek
     '''
+    if informacije[2] == 'video':   # če gre za TV oz. radio v živo
+        return 0
     r = requests.get(informacije[0])
     with open(f'{informacije[1]}.json', 'w') as datoteka:
         json.dump(informacije[4], datoteka, indent=4, ensure_ascii=False)
@@ -116,7 +124,7 @@ def pridobi_posnetek(povezava_do_html, n):
     izhod: informacije o posnetku: povezava do posnetka, ime, opis [tuple]
     gre za metaukaz, ki veriži pridobi_api, pridobi_json in info_json
     '''
-    povezava_do_api = pridobi_api(povezava_do_html, n)[0]
+    povezava_do_api = pridobi_api(povezava_do_html, n)
     informacije = info_json(pridobi_json(povezava_do_api, n), n)
     return informacije
 
@@ -157,9 +165,7 @@ def ukazna_vrstica():
     - shrani posnetek
       {IME_PROGRAMA}
     '''
-
     pomoč = ['-h', '--help', 'pomoč', '--pomoč']
-
     povezava_do_html = None
     ukaz = shrani_posnetek
     if (len(argv) == 1):
