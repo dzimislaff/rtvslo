d0e152ab8ade4e5db59a1ae5656ec794456ce2c8
nejc <nejc.mobilc@gmail.com>
2020-04-28 11:33:15 +0200

    urejeno zaganjanje izven domače mape

--- rtvslo/nastavitve.py
+++ rtvslo/nastavitve.py
@@ -5,6 +5,9 @@ import configparser
 import os
 
 
+# za zaganjanje programa izven domače mape
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
 client_id = '82013fb3a531d5414f478747c1aca622'
 session_id = '5cc765368952d4.22094713.245769764'
 
