e15be4bf79efaa1da0153fcd6a8293bf8c578c53
nejc <nejc.mobilc@gmail.com>
2020-04-30 11:53:20 +0200

    spremenjene deklaracije funkcij (NamedTuple -> Posnetek oz. Info); enojni narekovaji spremenjeni v dvojne

--- rtvslo/rtv.py
+++ rtvslo/rtv.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: 'UTF-8' -*-
+# -*- coding: "UTF-8" -*-
 
 import json
 import re
@@ -25,9 +25,9 @@ class Info(NamedTuple):
 
 def pridobi_spletno_stran(naslov: str
                           ) -> requests.models.Response:
-    '''
+    """
     zahteve: requests
-    '''
+    """
     try:
         return requests.get(naslov)
     except requests.exceptions.ConnectionError:
@@ -36,10 +36,10 @@ def pridobi_spletno_stran(naslov: str
 
 def pridobi_json(stran: requests.models.Response
                  ) -> dict:
-    '''
+    """
     zahteve: json
     pridobi informacije v obliki JSON o posnetku z api.rtvslo.si
-    '''
+    """
     if stran:
         return json.loads(stran.text)
     else:
@@ -48,17 +48,17 @@ def pridobi_json(stran: requests.models.Response
 
 def razberi_id(povezava_do_html: str
                ) -> str:
-    '''
+    """
     zahteve: re
     razbere številko posnetka z URL-povezave
-    '''
+    """
     try:
         assert type(povezava_do_html) == str
     except AssertionError:
         return None
 
-    štiride = re.compile(r'https?://4d\.rtvslo\.si/arhiv/\S+/\d{4,11}')
-    cifra = re.compile(r'\d{4,11}')
+    štiride = re.compile(r"https?://4d\.rtvslo\.si/arhiv/\S+/\d{4,11}")
+    cifra = re.compile(r"\d{4,11}")
     if štiride.search(povezava_do_html):
         return cifra.search(povezava_do_html).group()
     else:
@@ -68,9 +68,9 @@ def razberi_id(povezava_do_html: str
 def povezava_api_drm(številka: str,
                      client_id: str
                      ) -> str:
-    '''
+    """
     ustvari URL-povezavo do getRecordingDrm
-    '''
+    """
     povezava = (f"https://api.rtvslo.si/ava/getRecordingDrm/{številka}"
                 f"?client_id={client_id}")
     return povezava
@@ -80,19 +80,19 @@ def povezava_api_posnetek(številka: str,
                           client_id: str,
                           jwt: str
                           ) -> str:
-    '''
+    """
     vrne URL-povezavo do getMedia
-    '''
+    """
     povezava = (f"https://api.rtvslo.si/ava/getMedia/{številka}"
                 f"?client_id={client_id}&jwt={jwt}")
     return povezava
 
 
-def povezava_api_info(posnetek: NamedTuple
+def povezava_api_info(posnetek: Posnetek
                       ) -> str:
-    '''
+    """
     ustvari URL-povezavo do API getRecording
-    '''
+    """
     if posnetek.številka:
         povezava = (f"https://api.rtvslo.si/ava/getRecording/"
                     f"{posnetek.številka}?client_id={posnetek.client_id}")
@@ -103,35 +103,35 @@ def povezava_api_info(posnetek: NamedTuple
 
 def json_jwt(džejsn: dict
              ) -> str:
-    '''
+    """
     razbere jwt iz JSON-a
-    '''
+    """
     try:
-        return džejsn['response']['jwt']
+        return džejsn["response"]["jwt"]
     except KeyError:
         return None
 
 
 def json_povezava(džejsn: dict
                   ) -> str:
-    '''
+    """
     razbere URL-povezavo do posnetka iz JSON-a
-    '''
+    """
     try:
-        izbire = džejsn['response']['mediaFiles']
+        izbire = džejsn["response"]["mediaFiles"]
     except KeyError:
         return None
     if len(izbire) == 2:
-        if izbire[0]['height'] > izbire[1]['height']:
-            return izbire[0]['streams']['http']
+        if izbire[0]["height"] > izbire[1]["height"]:
+            return izbire[0]["streams"]["http"]
         else:
-            return izbire[1]['streams']['http']
+            return izbire[1]["streams"]["http"]
     else:
         try:
-            return izbire[0]['streams']['https']
+            return izbire[0]["streams"]["https"]
         except KeyError:
             try:
-                return izbire[0]['streams']['http']
+                return izbire[0]["streams"]["http"]
             except KeyError:
                 return None
 
@@ -139,44 +139,44 @@ def json_povezava(džejsn: dict
 def odstrani_znake(beseda: str,
                    nedovoljeni_znaki: list
                    ) -> str:
-    '''
+    """
     odstrani nedovoljene znake iz niza
-    '''
+    """
     try:
         assert type(beseda) == str
     except AssertionError:
         return None
 
-    beseda = beseda.replace(' ', '-')
-    nedovoljeni_znaki.append(',')
+    beseda = beseda.replace(" ", "-")
+    nedovoljeni_znaki.append(",")
     for i in nedovoljeni_znaki:
-        beseda = beseda.replace(i, '')
-    while '--' in beseda:
-        beseda = beseda.replace('--', '-')
-    beseda = beseda.lstrip('-').rstrip('-')
+        beseda = beseda.replace(i, "")
+    while "--" in beseda:
+        beseda = beseda.replace("--", "-")
+    beseda = beseda.lstrip("-").rstrip("-")
     return beseda
 
 
 def json_info(džejsn: dict,
               povezava_do_posnetka: str,
               n: dict
-              ) -> NamedTuple:
-    '''
+              ) -> Info:
+    """
     ustvari namedtuple s informacijami o posnetku
-    '''
+    """
     try:
-        naslov = džejsn['response']['title'].lower()
-        naslov = odstrani_znake(naslov, n['znaki'].split(','))
+        naslov = džejsn["response"]["title"].lower()
+        naslov = odstrani_znake(naslov, n["znaki"].split(","))
     except (KeyError, TypeError):
         naslov = None
 
     try:
-        mediatype = džejsn['response']['mediaFiles'][0]['mediaType'].lower()
+        mediatype = džejsn["response"]["mediaFiles"][0]["mediaType"].lower()
     except (KeyError, TypeError):
         mediatype = None
 
     try:
-        opis = džejsn['response']['description']
+        opis = džejsn["response"]["description"]
     except KeyError:
         opis = None
 
@@ -190,13 +190,13 @@ def json_info(džejsn: dict,
 def pridobi_posnetek(url: str,
                      n: dict,
                      številka: str = None
-                     ) -> NamedTuple:
-    '''
+                     ) -> Posnetek:
+    """
     metaukaz, ki zbere podatke, potrebne za predvajanje, shranjevanje posnetka
-    '''
+    """
     if not številka:
         številka = razberi_id(url)
-    client_id = n['client_id']
+    client_id = n["client_id"]
     jwt = json_jwt(pridobi_json(pridobi_spletno_stran(
         povezava_api_drm(številka,
                          client_id))))
@@ -211,31 +211,31 @@ def pridobi_posnetek(url: str,
 
 
 def zapiši_posnetek(stran: requests.models.Response,
-                    info: NamedTuple,
+                    info: Info,
                     cwd: str):
-    '''
+    """
     posnetek shrani v datoteko
-    '''
-    with open(f'{cwd}/{info.naslov}.{info.mediatype}', 'w+b') as datoteka:
+    """
+    with open(f"{cwd}/{info.naslov}.{info.mediatype}", "w+b") as datoteka:
         datoteka.write(stran.content)
 
 
-def zapiši_info(info: NamedTuple,
+def zapiši_info(info: Info,
                 cwd: str):
-    '''
+    """
     zahteve: json
     informacije o posnetku zapiše v datoteko
-    '''
-    with open(f'{cwd}/{info.naslov}.json', 'w') as datoteka:
+    """
+    with open(f"{cwd}/{info.naslov}.json", "w") as datoteka:
         json.dump(info.džejsn, datoteka, indent=4, ensure_ascii=False)
 
 
-def shrani_posnetek(posnetek: NamedTuple,
+def shrani_posnetek(posnetek: Posnetek,
                     n: dict,
                     cwd: str):
-    '''
+    """
     metaukaz, ki posnetek z informacijami shrani na disk
-    '''
+    """
     info = pridobi_informacije(posnetek, n, cwd)
     zapiši_info(info, cwd)
     zapiši_posnetek((pridobi_spletno_stran(info.povezava_do_posnetka)),
@@ -243,7 +243,7 @@ def shrani_posnetek(posnetek: NamedTuple,
                     cwd)
 
 
-def pridobi_informacije(posnetek: NamedTuple,
+def pridobi_informacije(posnetek: Posnetek,
                         n: dict,
                         cwd: str):
     if not posnetek.povezava_do_posnetka:
@@ -255,16 +255,16 @@ def pridobi_informacije(posnetek: NamedTuple,
     return info
 
 
-def predvajaj_posnetek(posnetek: NamedTuple,
+def predvajaj_posnetek(posnetek: Posnetek,
                        n: dict,
                        cwd=None):
-    '''
+    """
     zahteve: subprocces
     v zunanjem predvajalniku predvaja posnetek
-    '''
+    """
     if not posnetek.povezava_do_posnetka:
         pass
     else:
-        subprocess.call([n['predvajalnik'],
+        subprocess.call([n["predvajalnik"],
                          posnetek.povezava_do_posnetka,
-                         n['možnosti']])
+                         n["možnosti"]])
