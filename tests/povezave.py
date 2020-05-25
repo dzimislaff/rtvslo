87b276e6ca603988a8ffbe8391662572a44ad02e
nejc <nejc.mobilc@gmail.com>
2020-05-25 13:29:33 +0200

    posodobljene povezave, dodane nedelujoče

--- tests/povezave.py
+++ tests/povezave.py
@@ -5,8 +5,18 @@ seznam = [
     ('https://4d.rtvslo.si/arhiv/sedmi-dan/174681705'),
     ('https://4d.rtvslo.si/arhiv/pogled-na/174574657'),
     ('https://4d.rtvslo.si/arhiv/slovenski-film/60911866'),
-    ('https://4d.rtvslo.si/arhiv/dokumentarni-portret/6828'),
     ('https://4d.rtvslo.si/arhiv/dokumentarni-filmi-in-oddaje-kulturno-umetniski-program/174615520'),
     ('https://4d.rtvslo.si/arhiv/dokumentarni-filmi-in-oddaje-kulturno-umetniski-program/174369897'),
     ('https://4d.rtvslo.si/arhiv/prvi-na-maturi/174688153'),
+    ('https://4d.rtvslo.si/arhiv/meje-mojega-jezika-niso-meje-mojega-sveta/109826798'),
+    #
+    # nedelujoče
+    # opis: assert type(info.opis) == str (test_pridobi_informacije)
+    # ('https://4d.rtvslo.si/arhiv/res-je/117736682'),
+    #
+    # naslov: assert type(info.naslov) == str (test_pridobi_informacije)
+    # ('https://4d.rtvslo.si/arhiv/dokumentarni-portret/6828'),
+    #
+    # posnetek: assert type(posnetek.povezava_do_posnetka) (test_pridobi_posnetek)
+    # ('https://4d.rtvslo.si/arhiv/intervju-tv/155534620'),
 ]
