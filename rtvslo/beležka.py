c4cca4290525114bae51939d044ee561b55e3982
nejc <nejc.mobilc@gmail.com>
2021-02-23 20:00:43 +0100

    dodano prepoznavanje operacijskega sistema (linux, windows)

--- rtvslo/beležka.py
+++ rtvslo/beležka.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: 'UTF-8' -*-
 
-import subprocess
+import sys
+
+
+def prepoznaj_operacijski_sistem() -> bool:
+    """
+    smatram, da je na linuxu waylandova seja, sicer bi bil pyperclip dovolj
+    """
+    if sys.platform == "linux":
+        return True
 
 
 def beležka():
-    return subprocess.run(
-        ["wl-paste"], capture_output=True, text=True).stdout.strip("\n")
+    if prepoznaj_operacijski_sistem():
+        import subprocess
+        return subprocess.run(
+            ["wl-paste"], capture_output=True, text=True).stdout.strip("\n")
+    else:
+        import pyperclip
+        return pyperclip.paste()
