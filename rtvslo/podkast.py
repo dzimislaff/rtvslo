59c345cf3477c140041c6ac2966dade5abf39ea4
nejc <nejc.mobilc@gmail.com>
2022-01-23 13:06:41 +0100

    dodana Val 202 in Prvi

--- rtvslo/podkast.py
+++ rtvslo/podkast.py
@@ -8,35 +8,49 @@ import rtvslo.rtv
 import rtvslo.nastavitve
 
 
-def pridobi_seznam_povezav(povezava_do_podcasta: str):
-    r = rtvslo.rtv.Posnetek.pridobi_spletno_stran(povezava_do_podcasta)
+def pridobi_seznam_povezav(povezava_do_podkasta: str
+                           ) -> list:
+
+    def ustvari_povezave(ujemanje: str,
+                         stran: html.HtmlElement
+                         ) -> list:
+        radijski_ključ = '//h3[@class="post-excerpt__body__title"]/a//@href'
+        ključi = {
+            "ars": radijski_ključ,
+            "radioprvi": radijski_ključ,
+            "val202": radijski_ključ,
+            "365": '//h3[@class="title-cut-4-rows"]/a//@href'
+        }
+        konci_povezav = stran.xpath(ključi[ujemanje])
+        return [f"https://{ujemanje}.rtvslo.si{i}" for i in konci_povezav]
+
+    r = rtvslo.rtv.Posnetek.pridobi_spletno_stran(povezava_do_podkasta)
     stran = html.fromstring(r.content)
-    ključi = {
-        "ars": '//h3[@class="post-excerpt__body__title"]/a//@href',
-        "365": '//h3[@class="title-cut-4-rows"]/a//@href'
-    }
-    if "ars" in povezava_do_podcasta:
-        konci_povezav = stran.xpath(ključi["ars"])
-        povezave = [f"https://ars.rtvslo.si{i}" for i in konci_povezav]
-    elif "365." in povezava_do_podcasta:
-        konci_povezav = stran.xpath(ključi["365"])
-        povezave = [f"https://365.rtvslo.si{i}" for i in konci_povezav]
-    return povezave
+
+    if ujemanje := rtvslo.rtv.Posnetek.erteve.search(povezava_do_podkasta):
+        return ustvari_povezave(ujemanje.group(1), stran)
+    elif ujemanje := rtvslo.rtv.Posnetek.štiride.search(povezava_do_podkasta):
+        return ustvari_povezave(ujemanje.group(1), stran)
+    return []
 
 
-def main(povezava_do_podcasta: str):
+def main(povezava_do_podkasta: str):
     nastavitve = rtvslo.nastavitve.naloži_nastavitve()
     try:
         cwd = sys.argv[2]
     except IndexError:
         cwd = os.getcwd()
-    povezave = pridobi_seznam_povezav(povezava_do_podcasta)
-    for povezava in povezave:
-        posnetek = rtvslo.rtv.Posnetek(povezava, nastavitve)
-        try:
-            posnetek.shrani(cwd)
-        except rtvslo.rtv.NeveljavnaPovezava:
-            pass
+    povezave = pridobi_seznam_povezav(povezava_do_podkasta)
+
+    if povezave:
+        for povezava in povezave:
+            posnetek = rtvslo.rtv.Posnetek(povezava, nastavitve)
+            try:
+                posnetek.shrani(cwd)
+            except rtvslo.rtv.NeveljavnaPovezava:
+                pass
+    else:
+        print("Neveljavna povezava.")
 
 
 if __name__ == '__main__':
